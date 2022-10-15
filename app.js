41bd473a7c07be449e1cc17b7ba42f7ecfab7e33
Luqman Shaban <109437909+basemento@users.noreply.github.com>
2022-10-15 14:10:49 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+function Monday() {
+    lessons = ['DOM','Objects','Loops','functions']
+    computer = lessons[Math.floor(Math.random() * lessons.length)]
+    console.log(computer);
+}
+
+function Tuesday() {
+    lessons = ['DOM','Objects','Loops','functions']
+    computer = lessons[Math.floor(Math.random() * lessons.length)]
+    console.log(computer);
+}
+
+function Wednseday() {
+    lessons = ['DOM','Objects','Loops','functions']
+    computer = lessons[Math.floor(Math.random() * lessons.length)]
+    console.log(computer);
+}
+
+function Thursday() {
+    lessons = ['DOM','Objects','Loops','functions']
+    computer = lessons[Math.floor(Math.random() * lessons.length)]
+    console.log(computer);
+}
+
+function Friday() {
+    lessons = ['DOM','Objects','Loops','functions']
+    computer = lessons[Math.floor(Math.random() * lessons.length)]
+    console.log(computer);
+}
+
+function Saturday() {
+    lessons = ['DOM','Objects','Loops','functions']
+    computer = lessons[Math.floor(Math.random() * lessons.length)]
+    console.log(computer);
+}
+
+
+function Sunday() {
+    lessons = ['DOM','Objects','Loops','functions']
+    computer = lessons[Math.floor(Math.random() * lessons.length)]
+    console.log(computer);
+}
+
+//The code works by invoking a function using the day of the week's name
+
+
