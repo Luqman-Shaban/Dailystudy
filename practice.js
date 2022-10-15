79aeb1ff57da3f7dbf1044d3011417ae0ff15cff
Luqman Shaban <109437909+basemento@users.noreply.github.com>
2022-10-15 14:11:41 +0300

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,12 @@
+document.title = window.prompt('Enter your title');
+
+yob = (window.prompt('Enter your year of birth'));
+age = 2022 - yob
+ 
+if (age < 18) {
+    document.write('You are unatherized!')
+}
+
+else  {
+    document.write('You can use our services')
+};
